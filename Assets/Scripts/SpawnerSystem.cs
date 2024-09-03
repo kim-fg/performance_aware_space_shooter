54a1a576caa904de2db4711a72699795a41c9e83
Kim <joakim.linna@edu.futuregames.nu>
2024-09-03 11:52:23 +0200

    added project

---
+++ Assets/Scripts/SpawnerSystem.cs
@@ -0,0 +1,28 @@
+using Unity.Burst;
+using Unity.Entities;
+using Unity.Transforms;
+
+[BurstCompile]
+public partial struct SpawnerSystem : ISystem {
+    public void OnCreate(ref SystemState state) { }
+    public void OnDestroy(ref SystemState state) { }
+
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state) {
+        foreach (var spawner in SystemAPI.Query<RefRW<Spawner>>()) {
+            ProcessSpawner(ref state, spawner);
+        }
+    }
+
+    private void ProcessSpawner(ref SystemState state, RefRW<Spawner> spawner) {
+        if (spawner.ValueRO.NextSpawnTime < SystemAPI.Time.ElapsedTime) {
+            Entity newEntity = state.EntityManager.Instantiate(spawner.ValueRO.Prefab);
+            state.EntityManager.SetComponentData(
+                newEntity, 
+                LocalTransform.FromPosition(spawner.ValueRO.SpawnPosition)
+            );
+
+            spawner.ValueRW.NextSpawnTime = (float)SystemAPI.Time.ElapsedTime + spawner.ValueRO.SpawnRate;
+        }
+    }
+}
